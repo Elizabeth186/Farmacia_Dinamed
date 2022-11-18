0106c1432d7ddf49b7a1257354d1a763935f3c43
Elizabeth Suarez <89174310+Elizabeth186@users.noreply.github.com>
2022-11-18 13:49:37 -0600

    descarga de link de imagen storage

--- Screens/Agregar.js
+++ Screens/Agregar.js
@@ -63,11 +63,10 @@ const [url, seturl] = useState('');
 
     ref.on("jaja", function(){
       ref.snapshot.ref.getDownloadURL().then(function (urlimage){
-
+       seturl(urlimage)
         console.log(urlimage)
       })
     })
-
 
     try{
       await ref;
@@ -76,9 +75,6 @@ const [url, seturl] = useState('');
       console.log(e);
     }
     setuploading(false);
-    alert(
-      'imagen subida'
-    );
    
     setImage(source);
   };
@@ -122,10 +118,10 @@ const [url, seturl] = useState('');
       }
     }
   };
-  // const copyToClipboardurl = () => {
-  //   Clipboard.setString(ref && urlimage)
-  //   alert('Enlace copiado!!')
-  // }
+   const copyToClipboardurl = () => {
+     Clipboard.setString(url)
+     alert('Enlace copiado!!')
+   }
 
   // const copyToClipboard = () => {
   //   Clipboard.setString(image && image.uri)
@@ -189,10 +185,9 @@ const [url, seturl] = useState('');
              style={styles.tinyLogo}
              source={require('../assets/nube.png')}/>
       </TouchableOpacity>
-      {/* <TouchableOpacity style={styles.clipt} onPress={() => copyToClipboardurl()}>
-          <Text style={styles.cliptext}>jajja</Text>
-        </TouchableOpacity> */}
-
+       <TouchableOpacity style={styles.clipt} onPress={() => copyToClipboardurl()}>
+          <Text style={styles.cliptext}>{url}</Text>
+        </TouchableOpacity> 
       <Text style={styles.txtbtn} >Seleccione una imagen</Text>
       <View style={styles.btnview} >
         <TouchableOpacity  style={styles.btnview} onPress={pickimage}>
